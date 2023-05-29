546e0a8e6f7a6819765398da2b2f871829568712
cappeloow <cappe1996@hotmail.com>
2023-05-29 11:37:29 +0200

    added proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "api/": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
   plugins: [react()],
 })
