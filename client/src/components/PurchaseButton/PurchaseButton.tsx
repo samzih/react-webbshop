eed2ff353436244f68cfe4b819cf7b6b2253ff62
Carro <carolinaisabellaandersson@gmail.com>
2023-05-30 09:35:52 +0200

    change import

--- client/src/components/PurchaseButton/PurchaseButton.tsx
+++ client/src/components/PurchaseButton/PurchaseButton.tsx
@@ -1,12 +1,12 @@
 import { Button } from "antd";
 import { useCartContext } from "../../context/CartContext";
-import { Product } from "../HomePage/HomePage";
+import { IProduct } from "../../context/ProductContext";
 type Props = {
-  product: Product;
+  product: IProduct;
 };
 
 function PurchaseButton({ product }: Props) {
-  const { products, addProduct } = useCartContext();
+  const { addProduct } = useCartContext();
   return (
     <div>
       <Button onClick={() => addProduct(product)} type="primary">
