2a08224b9718dc3e6a2f4886c023f3a425424f0d
Carro <carolinaisabellaandersson@gmail.com>
2023-05-31 17:46:16 +0200

    code splitting of the cart into smaller components

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,24 +1,14 @@
-import { ShoppingCartOutlined, DeleteOutlined } from "@ant-design/icons";
-import { NavLink, Link } from "react-router-dom";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 import { useState } from "react";
-import { Badge, Button, Drawer, Space } from "antd";
-import type { DrawerProps } from "antd/es/drawer";
-import { Card } from "antd";
+import { Badge } from "antd";
 import { useCartContext } from "../context/CartContext";
+import CartPanel from "./CartPanel";
 
 function Cart() {
   const [open, setOpen] = useState(false);
-  const [placement] = useState<DrawerProps["placement"]>("right");
-  const { Meta } = Card;
-  const {
-    cart,
-    removeItem,
-    increaseCartQuantity,
-    decreaseCartQuantity,
-    totalSum,
-  } = useCartContext();
+  const { cart } = useCartContext();
 
-  const showDrawer = () => {
+  const showPanel = () => {
     setOpen(true);
   };
 
@@ -26,97 +16,15 @@ function Cart() {
     setOpen(false);
   };
 
-  const handleClick = (id: number) => {
-    removeItem(id);
-  };
-
-  const ButtonGroup = Button.Group;
-
-  // calculates total quantity of items in the cart
   let cartItemCount = 0;
-
-  cart.forEach((item) => {
-    cartItemCount += item.quantity;
-  });
+  cart.forEach((item) => (cartItemCount += item.quantity));
 
   return (
     <div>
-      <>
-        <Space>
-          <Badge offset={[0, 13]} count={cartItemCount}>
-            <ShoppingCartOutlined style={{ fontSize: 50 }} onClick={showDrawer} />
-          </Badge>
-        </Space>
-        <Drawer
-          title="KUNDVAGN"
-          placement={placement}
-          width={500}
-          onClose={onClose}
-          open={open}
-          extra={
-            <Space>
-              <NavLink to="./checkout">
-                <Button type="primary" disabled={cartItemCount <= 0} onClick={onClose}>
-                  Gå till kassan
-                </Button>
-              </NavLink>
-            </Space>
-          }
-        >
-          {cart.map((cartItem) => (
-            <div key={cartItem.product._id}>
-              <Link to={`/${cartItem.product._id}`} key={cartItem.product._id}>
-                <Card
-                  bordered
-                  size="small"
-                  hoverable
-                  style={{ width: 200, padding: 10, margin: 10 }}
-                  cover={<img alt="example" src={cartItem.product.image} />}
-                >
-                  <Meta
-                    title={cartItem.product.title}
-                    description={
-                      cartItem.product.price * cartItem.quantity + " kr"
-                    }
-                  />
-                  <DeleteOutlined
-                    onClick={() => handleClick(cartItem.product._id)}
-                  />
-
-                  <ButtonGroup>
-                    <Button
-                      type="primary"
-                      size="small"
-                      shape="circle"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        decreaseCartQuantity(cartItem.product);
-                      }}
-                      disabled={cartItem.quantity <= 1}
-                    >
-                      -
-                    </Button>
-                    <p>{cartItem.quantity}</p>
-                    <Button
-                      type="primary"
-                      size="small"
-                      shape="circle"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        increaseCartQuantity(cartItem.product);
-                      }}
-                      disabled={cartItem.quantity >= cartItem.product.inStock}
-                    >
-                      +
-                    </Button>
-                  </ButtonGroup>
-                </Card>
-              </Link>
-            </div>
-          ))}
-          <p>{`Totalsumma: ${totalSum} kr`}</p>
-        </Drawer>
-      </>
+      <Badge offset={[0, 13]} count={cartItemCount}>
+        <ShoppingCartOutlined style={{ fontSize: 50 }} onClick={showPanel} />
+      </Badge>
+      <CartPanel cartItemCount={cartItemCount} open={open} close={onClose} />
     </div>
   );
 }
