b2e9073b042f99c9369e178c10a6118f1ce18cb7
cappeloow <cappe1996@hotmail.com>
2023-06-01 11:28:44 +0200

    added some ant design on the registerform

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { PlusOutlined } from "@ant-design/icons";
+import "../component-styling/RegisterForm.css";
 import {
   Button,
   Col,
@@ -14,147 +15,10 @@ import {
 
 const { Option } = Select;
 
-const App: React.FC = () => {
-  const [open, setOpen] = useState(false);
-
-  const showDrawer = () => {
-    setOpen(true);
-  };
-
-  const onClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <>
-      <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>
-        New account
-      </Button>
-      <Drawer
-        title="Create a new account"
-        width={720}
-        onClose={onClose}
-        open={open}
-        bodyStyle={{ paddingBottom: 80 }}
-        extra={
-          <Space>
-            <Button onClick={onClose}>Cancel</Button>
-            <Button onClick={onClose} type="primary">
-              Submit
-            </Button>
-          </Space>
-        }
-      >
-        <Form layout="vertical" hideRequiredMark>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="name"
-                label="Name"
-                rules={[{ required: true, message: "Please enter user name" }]}
-              >
-                <Input placeholder="Please enter user name" />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="url"
-                label="Url"
-                rules={[{ required: true, message: "Please enter url" }]}
-              >
-                <Input
-                  style={{ width: "100%" }}
-                  addonBefore="http://"
-                  addonAfter=".com"
-                  placeholder="Please enter url"
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="owner"
-                label="Owner"
-                rules={[{ required: true, message: "Please select an owner" }]}
-              >
-                <Select placeholder="Please select an owner">
-                  <Option value="xiao">Xiaoxiao Fu</Option>
-                  <Option value="mao">Maomao Zhou</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="type"
-                label="Type"
-                rules={[{ required: true, message: "Please choose the type" }]}
-              >
-                <Select placeholder="Please choose the type">
-                  <Option value="private">Private</Option>
-                  <Option value="public">Public</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="approver"
-                label="Approver"
-                rules={[
-                  { required: true, message: "Please choose the approver" },
-                ]}
-              >
-                <Select placeholder="Please choose the approver">
-                  <Option value="jack">Jack Ma</Option>
-                  <Option value="tom">Tom Liu</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="dateTime"
-                label="DateTime"
-                rules={[
-                  { required: true, message: "Please choose the dateTime" },
-                ]}
-              >
-                <DatePicker.RangePicker
-                  style={{ width: "100%" }}
-                  getPopupContainer={(trigger) => trigger.parentElement!}
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <Form.Item
-                name="description"
-                label="Description"
-                rules={[
-                  {
-                    required: true,
-                    message: "please enter url description",
-                  },
-                ]}
-              >
-                <Input.TextArea
-                  rows={4}
-                  placeholder="please enter url description"
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
-      </Drawer>
-    </>
-  );
-};
-
 function RegisterForm() {
   const [open, setOpen] = useState(false);
-
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [form] = Form.useForm();
   const showDrawer = () => {
     setOpen(true);
   };
@@ -162,18 +26,10 @@ function RegisterForm() {
   const onClose = () => {
     setOpen(false);
   };
-  const [expandRegister, setExpandRegister] = useState(false);
-  // MÅSTE TYPA UPP DET
-  async function registerForm(e: any) {
-    e.preventDefault();
-    console.log(e);
-    const form = e.target;
-    console.log(form);
-    const firstName = form.elements.firstName.value;
-    const lastName = form.elements.lastName.value;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
 
+  // MÅSTE TYPA UPP DET
+  async function registerForm(values: any) {
+    const { firstName, lastName, email, password } = values;
     try {
       const response = await fetch("http://localhost:3000/api/users/register", {
         method: "POST",
@@ -189,6 +45,9 @@ function RegisterForm() {
       });
       const data = await response.json();
       console.log(data);
+      onClose();
+      form.resetFields();
+      setIsSubmitted(true);
     } catch {
       console.log(Error);
     }
@@ -208,28 +67,53 @@ function RegisterForm() {
         extra={
           <Space>
             <Button onClick={onClose}>Cancel</Button>
-            <Button onClick={onClose} type="primary">
-              Submit
-            </Button>
           </Space>
         }
       >
-        <form method="post" onSubmit={registerForm}>
-          <label>
-            <h4>registera här</h4>
-            <input name="firstName" placeholder="Förnamn" />
-          </label>
-          <label>
-            <input name="lastName" placeholder="Efternamn" />
-          </label>
-          <label>
-            <input name="email" placeholder="mejladress" />
-          </label>
-          <label>
-            <input name="password" type="password" placeholder="lösenord" />
-          </label>
-          <button type="submit">Submit form</button>
-        </form>
+        <Form form={form} onFinish={registerForm}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="firstName"
+                label="namn"
+                rules={[
+                  { required: true, message: "Please enter your first name" },
+                ]}
+              >
+                <Input placeholder="Johan" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="lastName"
+                label="efternamn"
+                rules={[
+                  { required: true, message: "Please enter your last name" },
+                ]}
+              >
+                <Input placeholder="Karlsson" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, message: "Please enter your email" }]}
+          >
+            <Input placeholder="Johan36@hotmail.com" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: "Please enter your password" }]}
+          >
+            <Input.Password placeholder="******" />
+          </Form.Item>
+          <Button htmlType="submit">Submit form</Button>
+          {isSubmitted && (
+            <p className="isSubmitedParagraf">Account created!</p>
+          )}
+        </Form>
       </Drawer>
     </div>
   );
