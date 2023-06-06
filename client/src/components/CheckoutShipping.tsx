2a3d2ff94c11b3233fd3228c84b2f4fbb3577b73
Sam <114141910+samzih@users.noreply.github.com>
2023-06-06 18:53:53 +0200

    Revert "changing so initial value on radio buttons update order"

--- client/src/components/CheckoutShipping.tsx
+++ client/src/components/CheckoutShipping.tsx
@@ -7,22 +7,21 @@ import { useOrderContext } from "../context/OrderContext";
 
 function CheckoutShipping() {
   const { shipping, calcDelivery } = useShippingContext();
-  const { totalSum } = useCartContext();
-  const { order, setOrder } = useOrderContext();
+  const {totalSum} = useCartContext()
   const [value, setValue] = useState(shipping[0]);
+  const { order, setOrder } = useOrderContext();
 
   const onChange = (e: RadioChangeEvent) => {
     console.log("radio checked", e.target.value);
     setValue(e.target.value);
-    setOrder({ ...order, shippingMethod: e.target.value._id });
+    setOrder({...order, shippingMethod: e.target.value._id})
   };
-
   return (
     <div>
-      <Radio.Group onChange={onChange} value={value}>
-        {shipping.map((shipping) => (
-          <div key={shipping._id}>
-            <Card>
+      {shipping.map((shipping) => (
+        <div key={shipping._id}>
+          <Card>
+            <Radio.Group onChange={onChange} value={value}>
               <Space direction="vertical">
                 <Card>
                   <Radio value={shipping}>{shipping.company}</Radio>
@@ -30,11 +29,11 @@ function CheckoutShipping() {
                   <p>Leveransdatum: {calcDelivery(shipping)} </p>
                 </Card>
               </Space>
-            </Card>
-          </div>
-        ))}
-        <p>Totalpris: {totalSum + value.price} </p>
-      </Radio.Group>
+            </Radio.Group>
+          </Card>
+        </div>
+      ))}
+      <p>Totalpris: {totalSum + value.price}  </p>
     </div>
   );
 }
