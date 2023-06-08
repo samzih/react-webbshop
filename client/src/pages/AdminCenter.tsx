08727f997418537563c10da131d6efb8dac4a89e
cappeloow <cappe1996@hotmail.com>
2023-06-08 13:42:45 +0200

    now we can't access the admin page without isAdmin true

--- client/src/pages/AdminCenter.tsx
+++ client/src/pages/AdminCenter.tsx
@@ -1,10 +1,15 @@
 import AdminCreateProduct from "../components/AdminCreateProduct";
+import { useUserContext } from "../context/UserContext";
+import { Card, Typography } from "antd";
 const AdminCenter = () => {
-  return (
+  const { loginUser } = useUserContext();
+  return loginUser && loginUser.isAdmin ? (
     <div>
       <h1>initial</h1>
       <AdminCreateProduct />
     </div>
+  ) : (
+    <h1>404 You can't access this page</h1>
   );
 };
 
