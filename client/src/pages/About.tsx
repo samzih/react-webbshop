962e0f5c9c4de3353948bba30086039b27cd91cc
Carro <carolinaisabellaandersson@gmail.com>
2023-06-15 23:08:38 +0200

    adding content and styling about and contact

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,11 +1,27 @@
+import "../Styling/About.css";
+import { Image } from "antd";
 export default function About() {
   return (
-    <>
+    <div className="about-container">
       <h2>Om oss</h2>
       <p>
         En liten kjosk i hjärtat av Göteborg som även tillhandahar produkter
         online.
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
-    </>
+      <Image
+        className="aboutImage responsiveImage"
+        width={600}
+        preview={false}
+        src="https://images.pexels.com/photos/12330187/pexels-photo-12330187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
+    </div>
   );
 }
--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,11 +1,20 @@
+import "../Styling/Contact.css";
+import { Image } from "antd";
 export default function Contact() {
   return (
-    <>
-      <h5>Information:</h5>
-      <p>Kjosk</p>
-      <p>Göteborg, Hamngatan 43</p>
-      <p>0733-00 00 00</p>
-      <p>enmejl@mejl.se</p>
-    </>
+    <div className="contact-container">
+      <div>
+        <h2 className="contact-title">Information:</h2>
+        <p>Kjosk</p>
+        <p>Göteborg, Hamngatan 43</p>
+        <p>0733-00 00 00</p>
+        <p>enmejl@mejl.se</p>
+      </div>
+      <Image
+        className="contactImage "
+        preview={false}
+        src="https://www.google.com/maps/vt/data=NoWqqusMxp1oO1ycCftvpYSHck9mB6DkmU7M6SX7PrWXD4hRjp5Q23XzBrpXv7xaOcG0LMXTd1ApmOdod1uOSSWg4vld3mhevNdTAP1TKKhFgPXyNYvuSbpm8OnpV3DTdbDDQ4P0EKkxeTi5J7nbsdC83hKFU0diFvQK"
+      />
+    </div>
   );
 }
